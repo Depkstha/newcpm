2fa42686414a60e78695fb03ad56a148ec7acf35
Depkstha <dharmarajshrestha9870@gmail.com>
2025-03-27 17:58:55 +0545

    first commit

---
+++ resources/js/pages/dashboard.tsx
@@ -0,0 +1,44 @@
+import { Button } from '@/components/ui/button';
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+import { toast } from 'sonner';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+];
+
+export default function Dashboard() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Dashboard" />
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                        <Button
+                            variant="outline"
+                            onClick={() =>
+                                toast('Event has been created', {
+                                    description: 'Sunday, December 03, 2023 at 9:00 AM',
+                                    action: {
+                                        label: 'Undo',
+                                        onClick: () => console.log('Undo'),
+                                    },
+                                })
+                            }
+                        >
+                            Show Toast
+                        </Button>
+                    </div>
+                </div>
+                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
+                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
